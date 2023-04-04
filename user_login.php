d514c1755b3b4b09d438e68d107ab477d2636470
Shaikh Masum <smasum1502@gmail.com>
2023-04-04 20:36:28 +0530

    Library Management System

---
+++ user_login.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Welcome to LogIn</title>
+      <link rel="stylesheet" href="css/signuplogin.css">
+   </head>
+   <body onload="load()">
+
+
+
+      <!-- PHP here-->
+
+
+
+      <!--loader here-->
+      <div id="loadbox">
+         <div class="center">
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+          </div>
+      </div>
+   
+
+
+
+      <div class="Box"><!-- Main Container Starts-->
+         <div class="mainbox"><!-- All Content Box-->
+            <div class="div left">
+               <h1 class="textalign font" style="margin-top:10px ;"><b>L O G I N</b></h1>
+               <p class="textalign text1"><b>Books are just a few clicks away!</b></p>
+               <div class="form" style="height:200px;"><!-- Signup Form starts here -->
+                  <form>
+                     <table>
+                        <tr><!-- For email starts-->
+                           <td class="text ftxtsiz">
+                              Enter Your Email:
+                           </td>
+                           <td>
+                              <input type="text" placeholder="Enter your email" name="email">
+                           </td>
+                        </tr><br><br><!-- Email ends-->
+                        <tr><!-- For password starts-->
+                           <td class="text ftxtsiz">
+                              Enter Your Password:
+                           </td>
+                           <td>
+                              <input type="password" placeholder="Enter Password" name="password">
+                           </td>
+                        </tr><!-- Password ends-->
+                        <tr><!-- For buttons starts-->
+                           <td>
+                           <button class="button text" type="reset">Reset</button>
+                           </td>
+                           <td>
+                           <button class="button text" type="submit" name="submit">LogIn</button>
+                           </td>
+                        </tr><!-- Buttons ends-->
+                     </table>
+                  </form>
+                  
+               </div><!-- form ends-->
+                  <p class="text1 ftxtsiz" style="color:black"><b>Forgot password?? <a class="a" href="#" >CLICK HERE</b></a><br><br>
+                  <p class="text1 ftxtsiz" style="color:black"><b>Don't have an account? <a class="a" href="user_signup.php"><br>Click here to SignUp!</a></b></p>
+            </div>
+            <div class="div right background right-radious">
+               <h2 class="h1 textalign texth shadow"> A platform for every reading need</h2>   
+            </div>
+         </div><!-- All Content Ends-->
+      </div><!-- Main Container Ends-->
+
+      <script>
+         var preloader = document.getElementById('loadbox');
+         
+         function load()
+         {
+            
+            preloader.style.display = 'none';
+         }
+      </script>
+
+   </body>
+
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   
+
+</html>
---
+++ user_signup.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Welcome to SignUp</title>
+      <link rel="stylesheet" href="../Bookreader/css/signuplogin.css">
+   </head>
+   <body onload="load()">
+
+
+      <!-- PHP HERE -->
+
+
+      <!--loader here-->
+      <div id="loadbox">
+         <div class="center">
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+          </div>
+      </div>
+
+
+      <div class="Box"><!-- Main Container Starts-->
+         <div class="mainbox"><!-- All Content Box-->
+            <div class="div left background left-radious">
+               <h2 class="h1 textalign texth shadow"> A platform for every reading need</h2>
+            </div>
+            <div class="div right">
+               <h1 class="textalign font" style="margin-top:10px ;"><b>R E G I S T E R</b></h1>
+               <p class="textalign text1"><b>Books are just a few clicks away!</b></p>
+               <div class="form"><!-- Signup Form starts here -->
+                  <form>
+                     <table>
+                        <tr><!-- For name starts-->
+                           <td class="text ftxtsiz">
+                              Name:
+                           </td>
+                           <td>
+                              <input type="text" placeholder="Enter your name" name="name">
+                           </td>
+                        </tr><!-- Name ends-->
+                        <tr><!-- For email starts-->
+                           <td class="text ftxtsiz">
+                              Email:
+                           </td>
+                           <td>
+                              <input type="text" placeholder="Enter your mail" name="email">
+                           </td>
+                        </tr><!-- Email ends-->
+                        <tr><!-- For Phone No starts-->
+                           <td class="text ftxtsiz">
+                              Phone Number:
+                           </td>
+                           <td>
+                              <input type="text" placeholder="Enter your Number" name="phone">
+                           </td>
+                        </tr><!-- Phone No ends-->
+                        <tr><!-- For gender starts-->
+                           <td class="text ftxtsiz">
+                              Gender:
+                           </td>
+                           <td class="text ftxtsiz">
+                              <div class="radio">
+                                 <input type="radio" value="m" name="gender" id="male">
+                                 <label for="male">Male</label>
+                     
+                                 <input type="radio" value="f" name="gender" id="female">
+                                 <label for="female">Female</label>
+                     
+                                 <input type="radio" value="o" name="gender" id="others">
+                                 <label for="others">Others</label>
+                              </div>
+                           </td>
+                        </tr><!-- Gender ends-->
+                        <tr><!-- For password starts-->
+                           <td class="text ftxtsiz">
+                              Password:
+                           </td>
+                           <td>
+                              <input type="password" placeholder="Enter Password" name="password">
+                           </td>
+                        </tr><!-- Password ends-->
+                        <tr><!-- For confirm password starts-->
+                           <td class="text ftxtsiz">
+                              Confirm Password:
+                           </td>
+                           <td>
+                              <input type="password" placeholder="Confirm the password" name="cpassword">
+                           </td>
+                        </tr><!-- Confirm password ends-->
+                        <tr><!-- For buttons starts-->
+                           <td>
+                           <button class="button text" type="reset">Reset</button>
+                           </td>
+                           <td>
+                           <button class="button text" type="submit" name="submit">Sign Up</button>
+                           </td>
+                        </tr><!-- Buttons ends-->
+                     </table>
+                  </form>
+               </div><!-- form ends-->
+                  <p class="text1 ftxtsiz" style="color:black"><b>Have an account? <a class="a" href="user_login.php">Click here to Login!</a></b></p>
+
+            </div>
+         </div><!-- All Content Ends-->
+      </div><!-- Main Container Ends-->
+
+
+      <script>
+         var preloader = document.getElementById('loadbox');
+         
+         function load()
+         {
+            
+            preloader.style.display = 'none';
+         }
+      </script>
+
+   </body>
+
+
+   
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf5d_Zx3AaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   
+</html>
